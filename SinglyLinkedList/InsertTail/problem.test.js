e560d079ab90111af9b072249fef0e2ecc8b396a
Fahad Hussain <sfahadh14@gmail.com>
2020-04-23 18:19:33 -0400

    adjusted test case for insertTail problem

--- SinglyLinkedList/InsertTail/problem.test.js
+++ SinglyLinkedList/InsertTail/problem.test.js
@@ -1,10 +1,23 @@
 const ll = require("../../Starter/starter");
 const InsertTail = require("./problem");
 
+const generateLinkedList = num => {
+    const linkedList = new ll.LinkedList();
+    for (let i = 1; i <= num; i++) {
+        linkedList.insert(new ll.Node(i))
+    }
+    return linkedList;
+}
+
 test("Return a linked list with 1 node", () => {
-    const oldList = new ll.LinkedList();
-    const newList = oldList;
-    const node = new ll.Node(4);
-    newList.insert(node)
-    expect(InsertTail(oldList, node)).toEqual(newList);
+    const outputList = new ll.LinkedList();
+
+    const tailNode = new ll.Node(4);
+    const node2 = new ll.Node(1);
+    const node3 = new ll.Node(2);
+    outputList.insert(node2);
+    outputList.insert(node3);
+    outputList.insert(tailNode);
+
+    expect(InsertTail(generateLinkedList(2), tailNode)).toEqual(outputList);
 })
