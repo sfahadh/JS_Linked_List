80ca00761865ff08f98861face01c133a3bccf8a
Fahad Hussain <sfahadh14@gmail.com>
2020-04-23 13:29:51 -0400

    added insertHead folder with problem file

---
+++ SinglyLinkedList/InsertHead/problem.js
@@ -0,0 +1,26 @@
+/*
+Complete the following Linked List insert head problem below. There are 2 inputs, the Linked List and the node in which you want to insert to the head of that Linked List. Once the node is inserted to the head, return the Linked List.
+
+function LinkedList() {
+    this.head = null;
+} 
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/
+
+const insertHead = (linkedList, node) => {
+
+}
+
+module.exports = insertTail;
+
+/*
+
+Linked List = { 3 => { 4 => { 5 => null } } }
+Input: { 2 => null }
+Expected Output: { 2 => { 3 => { 4 => { 5 => null } } } }
+
+*/
