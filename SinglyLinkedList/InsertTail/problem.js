56af658b21fa4acbe49de254bff2a3d1e0518c4b
Fahad Hussain <sfahadh14@gmail.com>
2020-04-24 12:59:20 -0400

    added 4 new test cases for InsertTail problem

--- SinglyLinkedList/InsertTail/problem.js
+++ SinglyLinkedList/InsertTail/problem.js
@@ -12,13 +12,7 @@ function Node(data, next = null) {
 */
 
 const insertTail = (linkedList, node) => {
-    let currNode = linkedList.head;
-    if (currNode) {
-        while (currNode.next) { currNode = currNode.next }
-        currNode.next = node;
-    } else linkedList.head = node;
-    
-    return linkedList;
+
 }
 
 module.exports = insertTail;
--- SinglyLinkedList/InsertTail/problem.test.js
+++ SinglyLinkedList/InsertTail/problem.test.js
@@ -3,7 +3,8 @@ const LinkedList = require("../../Starter/linkedList");
 const Node = require("../../Starter/node");
 const InsertTail = require("./problem");
 
-test("Return a linked list with 1 node", () => {
+// Test Case 1:
+test("Return a linked list with the node of data 4 attached to end", () => {
     const outputList = new LinkedList();
     generate.addNodesToLinkedList(outputList, 1, 2);
 
@@ -11,4 +12,47 @@ test("Return a linked list with 1 node", () => {
     outputList.insert(tailNode);
 
     expect(InsertTail(generate.generateLinkedList(1, 2), tailNode)).toEqual(outputList);
+})
+
+// Test Case 2:
+test("Return a linked list with the node of data 1 attached to end", () => {
+    const outputList = new LinkedList();
+
+    const tailNode = new Node(1);
+    outputList.insert(tailNode);
+
+    expect(InsertTail(new LinkedList(), tailNode)).toEqual(outputList);
+})
+
+// Test Case 3:
+test("Return a linked list with the node of data 8 attached to end", () => {
+    const outputList = new LinkedList();
+    generate.addNodesToLinkedList(outputList, 1, 7);
+
+    const tailNode = new Node(8);
+    outputList.insert(tailNode);
+
+    expect(InsertTail(generate.generateLinkedList(1, 7), tailNode)).toEqual(outputList);
+})
+
+// Test Case 4:
+test("Return a linked list with the node of data 6 attached to end", () => {
+    const outputList = new LinkedList();
+    generate.addNodesToLinkedList(outputList, 3, 5);
+
+    const tailNode = new Node(6);
+    outputList.insert(tailNode);
+
+    expect(InsertTail(generate.generateLinkedList(3, 5), tailNode)).toEqual(outputList);
+})
+
+// Test Case 5:
+test("Return a linked list with the node of data 1005 attached to end", () => {
+    const outputList = new LinkedList();
+    generate.addNodesToLinkedList(outputList, 34, 1000);
+
+    const tailNode = new Node(1005);
+    outputList.insert(tailNode);
+
+    expect(InsertTail(generate.generateLinkedList(34, 1000), tailNode)).toEqual(outputList);
 })
