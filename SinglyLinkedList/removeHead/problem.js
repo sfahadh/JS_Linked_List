7e3375609dce3d6220767bb64892ea1d6f26877f
Fahad Hussain <sfahadh14@gmail.com>
2020-04-25 15:57:57 -0400

    added removeHead problem

---
+++ SinglyLinkedList/removeHead/problem.js
@@ -0,0 +1,26 @@
+/*
+Complete the following Linked List remove head problem below. There is only 1 input, which is the linkedlist. Remove the first node and once that's done, return the Linked List.
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/
+
+
+const removeHead = linkedList => {
+    
+}
+
+module.exports = removeHead;
+
+/*
+
+Linked List = { 3 => { 4 => { 5 => null } } }
+Expected Output: { 4 => { 5 => null } } 
+
+*/
