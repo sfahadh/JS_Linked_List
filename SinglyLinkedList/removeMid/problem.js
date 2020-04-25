387629f59da255dbcdf78fc80cd0f3713842fd47
Fahad Hussain <sfahadh14@gmail.com>
2020-04-25 16:57:05 -0400

    added removeMid problem

---
+++ SinglyLinkedList/removeMid/problem.js
@@ -0,0 +1,25 @@
+/*
+Complete the following Linked List remove mid problem below. There is only 1 input, which is the linkedlist. Remove the middle node and once that's done, return the Linked List.
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/ 
+
+
+const removeMid = linkedList => {
+
+}
+
+module.exports = removeMid;
+
+/*
+
+Linked List = { 3 => { 4 => { 5 => null } } }
+Expected Output: { 3 => { 5 => null } } 
+*/
