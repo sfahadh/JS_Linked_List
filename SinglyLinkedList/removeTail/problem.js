677391620db03b969effa0b225a712f8290f50fc
Fahad Hussain <sfahadh14@gmail.com>
2020-04-25 16:18:42 -0400

    added removeTail problem

---
+++ SinglyLinkedList/removeTail/problem.js
@@ -0,0 +1,26 @@
+/*
+Complete the following Linked List remove tail problem below. There is only 1 input, which is the linkedlist. Remove the last node and once that's done, return the Linked List.
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/
+
+
+const removeTail = linkedList => {
+  
+}
+
+module.exports = removeTail;
+
+/*
+
+Linked List = { 3 => { 4 => { 5 => null } } }
+Expected Output: { 3 => { 4 => null } }
+
+*/
