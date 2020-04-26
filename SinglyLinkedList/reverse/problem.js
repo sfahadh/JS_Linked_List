3024d5093f5aa68cbe2880ac2d76a6ec12567cef
Fahad Hussain <sfahadh14@gmail.com>
2020-04-26 18:38:26 -0400

    added reverse problem

---
+++ SinglyLinkedList/reverse/problem.js
@@ -0,0 +1,26 @@
+/*
+Complete the following Linked List reverse problem below. There is only 1 input, which is the Linked List and the goal is to reverse the List List so that the head end will now be the tail and and vice versa. 
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/
+
+
+const reverse = linkedList => {
+
+}
+
+module.exports = reverse;
+
+/*
+
+Linked List = { 3 => { 4 => { 5 => { 6 => null } } } }
+Expected Output: { 6 => { 5 => { 4 => { 3 = > } } } }
+
+*/
