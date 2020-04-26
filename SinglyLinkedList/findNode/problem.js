38caa239ca94784550649f74c253fe6b6326b73a
Fahad Hussain <sfahadh14@gmail.com>
2020-04-26 17:44:30 -0400

    added findNode problem

---
+++ SinglyLinkedList/findNode/problem.js
@@ -0,0 +1,31 @@
+/*
+Complete the following Linked List find node problem below. There are 2 inputs, the Linked List and the node data that may or may not be found in the Linked List. Traverse through the Linked List and if the node data is found, return true, otherwise return false.
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/
+
+
+const findNode = (linkedList, data) => {
+    
+}
+
+module.exports = findNode;
+
+/*
+
+Linked List = { 3 => { 4 => { 5 => null } } }
+Input: Linked List, 4
+Expected Output: true
+
+Linked List = { 3 => { 4 => { 5 => null } } }
+Input: Linked List, 7
+Expected Output: false
+
+*/
