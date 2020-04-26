8ad32195b3f5f4deff698295cb1c4ec248d3965d
Fahad Hussain <sfahadh14@gmail.com>
2020-04-26 18:04:05 -0400

    added getAllNodes problem

---
+++ SinglyLinkedList/getAllNodes/problem.js
@@ -0,0 +1,26 @@
+/*
+Complete the following Linked List get all nodes problem below. There is only 1 input, which is the Linked List and the goal is to retrieve all node data and return the result.
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/
+
+
+const findNode = linkedList => {
+
+}
+
+module.exports = findNode;
+
+/*
+
+Linked List = { 3 => { 4 => { 5 => null } } }
+Expected Output: [3, 4, 5]
+
+*/
