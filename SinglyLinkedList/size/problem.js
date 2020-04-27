f72c6959009a36662ebb0e8a757c81028f982d90
Fahad Hussain <sfahadh14@gmail.com>
2020-04-26 23:51:30 -0400

    added size problem

---
+++ SinglyLinkedList/size/problem.js
@@ -0,0 +1,27 @@
+/*
+Complete the following Linked List size problem below. There is only 1 input, which the Linked List. The goal is the find the number of nodes the Linked List has.
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/
+
+
+const size = linkedList => {
+
+}
+
+module.exports = size;
+
+/*
+
+Linked List = { 3 => { 4 => { 5 => null } } }
+Expected Output: 3
+
+
+*/
