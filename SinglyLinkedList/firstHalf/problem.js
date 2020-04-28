fef918c2a866fc74941fc5ee7419e0814500c76f
Fahad Hussain <sfahadh14@gmail.com>
2020-04-27 22:48:31 -0400

    added firstHalf problem

---
+++ SinglyLinkedList/firstHalf/problem.js
@@ -0,0 +1,28 @@
+/*
+Complete the following Linked List first half problem below. There is only 1 input, which is the Linked List and the goal is to ignore the second half of the Linked List and return the first half.
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/
+
+const firstHalf = linkedList => {
+
+}
+
+module.exports = firstHalf;
+
+/*
+
+Linked List = { 3 => { 4 => { 5 => { 6 => null } } } }
+Expected Output: { 3 => { 4 => null } } 
+
+Linked List = { 3 => { 4 => { 5 => null } } }
+Expected Output: { 3 => { 4 => null } } 
+
+*/
