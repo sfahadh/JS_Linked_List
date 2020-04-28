29b7559456f5d393141a5782250f65ecf000d1ec
Fahad Hussain <sfahadh14@gmail.com>
2020-04-27 22:54:38 -0400

    added test cases for firstHalf problem

--- SinglyLinkedList/firstHalf/problem.test.js
+++ SinglyLinkedList/firstHalf/problem.test.js
@@ -2,36 +2,36 @@ const generate = require("../../Starter/generate");
 const SecondHalf = require("./problem");
 
 // Test Case 1:
-test("Return second half of linked list with datas of 4 - 5", () => {
+test("Return first half of linked list with datas of 3 - 4", () => {
     const inputList = generate.generateLinkedList(3, 5);
-    const outputList = generate.generateLinkedList(4, 5);
+    const outputList = generate.generateLinkedList(3, 4);
     expect(SecondHalf(inputList)).toEqual(outputList.head);
 })
 
 // Test Case 2:
-test("Return second half of linked list with only 1 node", () => {
+test("Return first half of linked list with only 1 node", () => {
     const inputList = generate.generateLinkedList(8, 9);
-    const outputList = generate.generateLinkedList(9, 9);
+    const outputList = generate.generateLinkedList(8, 8);
     expect(SecondHalf(inputList)).toEqual(outputList.head);
 })
 
 // Test Case 3:
-test("Return second half of linked list with datas of 62 - 70", () => {
+test("Return second half of linked list with datas of 54 - 62", () => {
     const inputList = generate.generateLinkedList(54, 70);
-    const outputList = generate.generateLinkedList(62, 70);
+    const outputList = generate.generateLinkedList(54, 62);
     expect(SecondHalf(inputList)).toEqual(outputList.head);
 })
 
 // Test Case 4:
-test("Return second half of linked list with datas of 5 - 6", () => {
+test("Return second half of linked list with datas of 3 - 4", () => {
     const inputList = generate.generateLinkedList(3, 6);
-    const outputList = generate.generateLinkedList(5, 6);
+    const outputList = generate.generateLinkedList(3, 4);
     expect(SecondHalf(inputList)).toEqual(outputList.head);
 })
 
 // Test Case 5:
 test("Return second half of linked list with datas of 6 - 10", () => {
     const inputList = generate.generateLinkedList(1, 10);
-    const outputList = generate.generateLinkedList(6, 10);
+    const outputList = generate.generateLinkedList(1, 5);
     expect(SecondHalf(inputList)).toEqual(outputList.head);
 })
