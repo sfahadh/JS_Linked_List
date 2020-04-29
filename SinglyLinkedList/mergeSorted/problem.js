ec492864c9c539a2c73b2ca754cd8e8772b2113e
Fahad Hussain <sfahadh14@gmail.com>
2020-04-28 23:36:07 -0400

    added mergeSorted problem

---
+++ SinglyLinkedList/mergeSorted/problem.js
@@ -0,0 +1,31 @@
+/* // Uncomment the imports below if you plan on using them
+const LinkedList = require("../../Starter/linkedList");
+const Node = require("../../Starter/node");
+*/
+
+/*
+Complete the following Linked List merge sorted problem below. There are 2 inputs, both being two sorted Linked Lists. The goal is to combine the two sorted Linked Lists into 1 sorted Linked List.
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null) {
+    this.data = data;
+    this.next = next;
+}
+*/
+
+const mergeSorted = linkedList => {
+
+}
+
+module.exports = mergeSorted;
+
+/*
+
+Linked List 1 = { 24 => { 42 => null } }
+Linked List 2 = { 3 => { 29 => null } } 
+Expected Output: { 3 => { 24 => { 29 => { 42 => null } } } }
+
+*/
