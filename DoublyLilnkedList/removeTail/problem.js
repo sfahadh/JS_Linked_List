372b14d82aad1ae743cc948c554ddf39ec39a4cd
Fahad Hussain <sfahadh14@gmail.com>
2020-07-05 20:45:47 -0400

    added problem file for removeAt problem in doublylinkedlistfolder

---
+++ DoublyLilnkedList/removeTail/problem.js
@@ -0,0 +1,33 @@
+// ? In Visual Studo Code, install "Better Comments" to see colorful effects of comments
+
+/* // Uncomment the imports below if you plan on using them
+* const LinkedList = require("../../Starter/Doubly/linkedList");
+* const Node = require("../../Starter/Doubly/node");
+*/
+
+/*
+TODO: Complete the following Linked List remove tail problem below. There is only 1 input, which is the linkedlist. Remove the last node and once that's done, return the Linked List.
+
+function LinkedList() {
+    this.head = null;
+}
+
+function Node(data, next = null, prev = null) {
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+}
+*/
+
+
+const removeTail = linkedList => {
+
+}
+
+module.exports = removeTail;
+
+/*
+! Test Case: 
+Linked List = { 3 <=> { 4 <=> { 5 => null } } }
+Expected Output: { 3 <=> { 4 => null } }
+*/
